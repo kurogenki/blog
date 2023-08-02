701177a65c32dd520a4f046b9ae1644209c28061
kuroGenki <kurochange0522@gmail.com>
2023-08-03 02:32:53 +0900

    新規作成処理の実装

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@ use App\Http\Controllers\PostController;
 */
     Route::get('/posts', [PostController::class, 'index']);
     Route::get('/', [PostController::class, 'index']);
+    Route::get('/posts/create', [PostController::class ,'create']);
+    Route::post('/posts', [PostController::class, 'store']);
     Route::get('/posts/{post}', [PostController::class ,'show']);
 // '/posts/{対象データのID}'にGetリクエストが来たら、PostControllerのshowメソッドを実行する
 
